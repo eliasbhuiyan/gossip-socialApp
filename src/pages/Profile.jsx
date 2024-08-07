99cedc2bef706bbd72c9355727c20af41f4ee616
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2024-08-07 19:26:08 +0600

    profile upload

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,18 @@
 import React, { createRef, useState } from "react";
 import { CiCirclePlus } from "react-icons/ci";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Cropper from "react-cropper";
 import "cropperjs/dist/cropper.css";
 import { GiCrossMark } from "react-icons/gi";
-
+import { getDownloadURL, getStorage, ref, uploadString } from "firebase/storage";
+import { getDatabase,ref as dataref, child, push, update } from "firebase/database";
+import { loggedUserData } from "../reducer/userSlice";
+import { toast, ToastContainer } from "react-toastify";
 const Profile = () => {
   const loggedUser = useSelector((state) => state.loggedUser.user);
-
+  const db = getDatabase();
+  const storage = getStorage();
+  const dispatch = useDispatch();
   const cropperRef = createRef();
   const [image, setImage] = useState("");
   const [cropData, setCropData] = useState("");
@@ -38,8 +43,32 @@ const Profile = () => {
     setImage("")
     setCropData("")
   }
+
+  const handelUpload = ()=>{
+  
+    uploadString(ref(storage, loggedUser.uid), cropData, 'data_url').then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((downloadURL) => {
+        update(dataref(db, 'users/' + loggedUser.uid),{
+          photoURL: downloadURL,
+        }).then(()=>{
+          dispatch(loggedUserData({...loggedUser, photoURL: downloadURL}))
+          setImage("")
+          setCropData("")
+          toast.success(
+            "Profile picture uploaded successfully!"
+          );
+        })
+      });
+    });
+  }
+  console.log();
   return (
     <div className="p-10 bg-[#F4F4F4] w-fit flex flex-col items-center gap-10 m-auto mt-24 shadow">
+      <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          theme="light"
+        ></ToastContainer>
       {
         image &&
         <div className="absolute top-0 left-1/2 -translate-x-1/2 w-fit bg-brand z-50">
@@ -66,21 +95,27 @@ const Profile = () => {
             }}
             guides={true}
           />
-          <div className="flex justify-center py-4">
+          <div className="flex justify-center py-4 gap-3">
             <button onClick={getCropData} className="text-brand py-3 px-5 bg-white rounded-xl font-semibold">
                 Crop Image
             </button>
+            {
+              cropData &&
+            <button onClick={handelUpload} className="text-white py-3 px-5 bg-green-600 rounded-xl font-semibold">
+              Upload
+            </button>
+            }
           </div>
           {
             cropData &&
             <div className="w-24 h-24 relative rounded-full overflow-hidden m-auto border-2 border-white mb-4">
-              <img src={cropData} alt="" className="w-full"/>
+              <img src={cropData} alt="" className="w-full h-full"/>
             </div>
           }
       </div>
       }
       <div className="w-24 h-24 relative rounded-full overflow-hidden group">
-        <img src={loggedUser?.photoURL} className="w-full" alt="profile" />
+        <img src={loggedUser?.photoURL} className="w-full h-full" alt="profile" />
         <label htmlFor="img" className="w-full h-full scale-0 group-hover:scale-100 transition-all cursor-pointer absolute top-0 left-0 bg-[rgba(0,0,0,0.5)] flex items-center justify-center">
           <CiCirclePlus className="text-3xl text-white" />
           <input id="img" type="file" onChange={handelChange} className="hidden"/>
